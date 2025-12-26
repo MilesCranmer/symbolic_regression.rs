a1d5141127090d0da1ad5814551d9a838ab32903
MilesCranmer <miles.cranmer@gmail.com>
2025-12-26 21:37:33 +0000

    test: add ternary infix operator test

--- dynamic_expressions/tests/test_strings.rs
+++ dynamic_expressions/tests/test_strings.rs
@@ -2,6 +2,52 @@ mod common;
 
 use common::expr_readme_like;
 use dynamic_expressions::strings::{StringTreeOptions, string_tree};
+use dynamic_expressions::{HasOp, Metadata, PNode, PostfixExpr, custom_opset};
+
+custom_opset! {
+    struct Infix3Ops<f64> {
+        2 {
+            add {
+                display: "+",
+                infix: "+",
+                commutative: true,
+                associative: true,
+                eval(args) { args[0] + args[1] },
+                partial(_args, _idx) { 1.0 },
+            }
+        }
+        3 {
+            oplus {
+                display: "⊕",
+                infix: "⊕",
+                eval(args) { args[0] + args[1] + args[2] },
+                partial(_args, _idx) { 1.0 },
+            }
+        }
+    }
+}
+
+fn var_infix3(feature: u16) -> PostfixExpr<f64, Infix3Ops, 3> {
+    PostfixExpr::new(vec![PNode::Var { feature }], vec![], Metadata::default())
+}
+
+fn add_infix3(x: PostfixExpr<f64, Infix3Ops, 3>, y: PostfixExpr<f64, Infix3Ops, 3>) -> PostfixExpr<f64, Infix3Ops, 3> {
+    dynamic_expressions::expression_algebra::__apply_postfix::<f64, Infix3Ops, 3, 2>(
+        <Infix3Ops as HasOp<Infix3OpsAdd>>::ID,
+        [x, y],
+    )
+}
+
+fn oplus3(
+    x: PostfixExpr<f64, Infix3Ops, 3>,
+    y: PostfixExpr<f64, Infix3Ops, 3>,
+    z: PostfixExpr<f64, Infix3Ops, 3>,
+) -> dynamic_expressions::PostfixExpr<f64, Infix3Ops, 3> {
+    dynamic_expressions::expression_algebra::__apply_postfix::<f64, Infix3Ops, 3, 3>(
+        <Infix3Ops as HasOp<Infix3OpsOplus>>::ID,
+        [x, y, z],
+    )
+}
 
 #[test]
 fn string_tree_matches_expected() {
@@ -28,4 +74,21 @@ fn infix_preserves_child_parens_for_precedence() {
     let expr = (a + b) * c;
     let s = string_tree(&expr, StringTreeOptions::default());
     assert_eq!(s, "(a + b) * c");
+}
+
+#[test]
+fn ternary_infix_custom_op_preserves_child_grouping() {
+    let mut a = var_infix3(0);
+    a.meta.variable_names = vec!["a".into(), "b".into(), "c".into()];
+    let b = var_infix3(1);
+    let c = var_infix3(2);
+
+    // (a + b) ⊕ (b + c) ⊕ a
+    //
+    // If the "infix join" path strips child outer parens, this can degrade to:
+    // a + b ⊕ b + c ⊕ a  (ambiguous / wrong-looking).
+    let expr = oplus3(add_infix3(a.clone(), b.clone()), add_infix3(b, c), a);
+
+    let s = string_tree(&expr, StringTreeOptions::default());
+    assert_eq!(s, "(a + b) ⊕ (b + c) ⊕ a");
 }
